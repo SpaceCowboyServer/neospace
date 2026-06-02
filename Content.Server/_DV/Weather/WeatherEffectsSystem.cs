d56f11f5c06d28370d89a1c3f0c21a0b43114a84
Dan <jamasty@protonmail.com>
2026-06-02 14:44:09 +0300

    dynamic weather and day cycle to glacier

---
+++ Content.Server/_DV/Weather/WeatherEffectsSystem.cs
@@ -0,0 +1,120 @@
+using Content.Server._DV.Weather;
+using Content.Shared.Damage;
+using Content.Shared.Damage.Systems;
+using Content.Shared.Mobs;
+using Content.Shared.Mobs.Components;
+using Content.Shared.StatusEffectNew; // starcup
+using Content.Shared.StatusEffectNew.Components;
+using Content.Shared.Weather;
+using Content.Shared.Whitelist;
+using Robust.Shared.Map.Components;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Timing;
+
+namespace Content.Shared._DV.Weather;
+
+/// <summary>
+/// Handles weather damage for exposed entities.
+/// </summary>
+public sealed partial class WeatherEffectsSystem : EntitySystem
+{
+    [Dependency] private DamageableSystem _damageable = default!;
+    [Dependency] private EntityWhitelistSystem _whitelist = default!;
+    [Dependency] private IGameTiming _timing = default!;
+    // [Dependency] private readonly IPrototypeManager _proto = default!; // starcup: unused
+    [Dependency] private SharedMapSystem _map = default!;
+    [Dependency] private SharedWeatherSystem _weather = default!;
+    [Dependency] private StatusEffectsSystem _statusEffects = default!; // starcup
+
+    private EntityQuery<MapGridComponent> _gridQuery;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        _gridQuery = GetEntityQuery<MapGridComponent>();
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+        var query = EntityQueryEnumerator<WeatherDamageStatusEffectComponent, StatusEffectComponent>();
+        var now = _timing.CurTime;
+        while (query.MoveNext(out var effectEnt, out var weather, out var status))
+        {
+            if (!status.Applied)
+                continue;
+
+            if (status.AppliedTo is not {} map)
+                continue;
+
+            if (now < weather.NextUpdate)
+                continue;
+
+            weather.NextUpdate = now + weather.UpdateDelay;
+
+            // FIXME: determine how weather startup and ending should be handled now
+            // start and end do no damage
+            // if (data.State != WeatherState.Running)
+            //     continue;
+
+            UpdateDamage(map, weather);
+            UpdateEffects(map, weather); // starcup
+        }
+    }
+
+    private void UpdateDamage(EntityUid map, WeatherDamageStatusEffectComponent weather)
+    {
+        if (weather.Damage is not {} damage)
+            return;
+
+        var query = EntityQueryEnumerator<MobStateComponent, TransformComponent>();
+        while (query.MoveNext(out var uid, out var mob, out var xform))
+        {
+            // don't give dead bodies 10000 burn, that's not fun for anyone
+            if (xform.MapUid != map || mob.CurrentState == MobState.Dead)
+                continue;
+
+            // if not in space, check for being indoors
+            if (xform.GridUid is {} gridUid && _gridQuery.TryComp(gridUid, out var grid))
+            {
+                var tile = _map.GetTileRef((gridUid, grid), xform.Coordinates);
+                if (!_weather.CanWeatherAffect((gridUid, grid), tile))
+                    continue;
+            }
+
+            if (_whitelist.IsWhitelistFailOrNull(weather.DamageBlacklist, uid))
+                _damageable.TryChangeDamage(uid, damage, interruptsDoAfters: false);
+        }
+    }
+
+    // starcup - allows weather to inflict status effects
+    private void UpdateEffects(EntityUid map, WeatherDamageStatusEffectComponent weather)
+    {
+        if (weather.StatusEffect is not {} statusEffectId)
+            return;
+
+        var query = EntityQueryEnumerator<MobStateComponent, TransformComponent>();
+        while (query.MoveNext(out var uid, out var mob, out var xform))
+        {
+            if (xform.MapUid != map || mob.CurrentState == MobState.Dead)
+                continue;
+
+            // if not in space, check for being indoors
+            if (xform.GridUid is {} gridUid && _gridQuery.TryComp(gridUid, out var grid))
+            {
+                var tile = _map.GetTileRef((gridUid, grid), xform.Coordinates);
+                if (!_weather.CanWeatherAffect((gridUid, grid), tile))
+                    continue;
+            }
+
+            if (weather.Refresh)
+                _statusEffects.TryUpdateStatusEffectDuration(uid, statusEffectId, TimeSpan.FromSeconds(10));
+            else
+                _statusEffects.TryAddStatusEffectDuration(uid, statusEffectId, TimeSpan.FromSeconds(10));
+
+        }
+    }
+}
+
+
---
+++ Content.Server/_starcup/Weather/DynamicWeatherSystem.cs
@@ -0,0 +1,105 @@
+using System.Linq;
+using Content.Shared.Random.Helpers;
+using Content.Shared.Weather;
+using Robust.Shared.Map.Components;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Random;
+using Robust.Shared.Timing;
+
+namespace Content.Server._starcup.Weather;
+
+public sealed partial class DynamicWeatherSystem : EntitySystem
+{
+    [Dependency] private IGameTiming _gameTiming = default!;
+    [Dependency] private IRobustRandom _robustRandom = default!;
+    [Dependency] private SharedWeatherSystem _weather = default!;
+    [Dependency] private IPrototypeManager _proto = default!;
+
+    /// <summary>
+    /// The meta-weather type that functions as a stand-in for no active weather event.
+    /// </summary>
+    private static readonly EntProtoId<WeatherStatusEffectComponent> WeatherClear = "WeatherClear";
+
+    private static readonly TimeSpan MaximumExpectedRoundLength = TimeSpan.FromHours(6);
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<DynamicWeatherComponent, MapInitEvent>(OnMapInit);
+    }
+
+    private void OnMapInit(EntityUid entity, DynamicWeatherComponent dynamicWeather, MapInitEvent args)
+    {
+        if (!_proto.Resolve(dynamicWeather.Scheduler, out var weatherScheduler))
+            return;
+
+        EntProtoId<WeatherStatusEffectComponent>? initialStateProtoId = weatherScheduler.States.First().Key;
+        if (dynamicWeather.RandomInitialState)
+        {
+            for (var i = 0; i < MaximumExpectedRoundLength / dynamicWeather.StepFrequency; i++)
+            {
+                initialStateProtoId = NextState(dynamicWeather, weatherScheduler);
+                dynamicWeather.CurrentState = initialStateProtoId;
+            }
+        }
+
+        if (initialStateProtoId == WeatherClear)
+            initialStateProtoId = null;
+
+        SetWeather(entity, dynamicWeather, initialStateProtoId);
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var now = _gameTiming.CurTime;
+        var query = EntityQueryEnumerator<DynamicWeatherComponent, MapComponent>();
+        while (query.MoveNext(out var entity, out var dynamicWeather, out var map))
+        {
+            if (now < dynamicWeather.NextUpdate)
+                continue;
+
+            dynamicWeather.NextUpdate = now + dynamicWeather.StepFrequency;
+
+            if (!_proto.Resolve(dynamicWeather.Scheduler, out var weatherScheduler))
+                continue;
+
+            SetWeather(entity, dynamicWeather, NextState(dynamicWeather, weatherScheduler));
+        }
+    }
+
+    private EntProtoId<WeatherStatusEffectComponent>? NextState(DynamicWeatherComponent dynamicWeather, WeatherSchedulerPrototype weatherScheduler)
+    {
+        var currentStateProto = dynamicWeather.CurrentState ?? WeatherClear;
+        return _robustRandom.Pick(weatherScheduler.States[currentStateProto].Transitions);
+    }
+
+    private void SetWeather(EntityUid map, DynamicWeatherComponent dynamicWeather, EntProtoId<WeatherStatusEffectComponent>? weatherProto)
+    {
+        if (weatherProto != null && weatherProto.Equals(WeatherClear))
+            weatherProto = null;
+
+        var previousState = dynamicWeather.CurrentState;
+        dynamicWeather.CurrentState = weatherProto;
+
+        var mapId = Transform(map).MapID;
+        _weather.TrySetWeather(mapId, weatherProto, out _, dynamicWeather.NextUpdate + SharedWeatherSystem.ShutdownTime);
+
+        if (!_proto.Resolve(dynamicWeather.Scheduler, out var weatherScheduler))
+            return;
+
+        if (previousState == weatherProto)
+            return;
+
+        var ev = new DynamicWeatherUpdateEvent(map, previousState, weatherProto);
+        RaiseLocalEvent(map, ref ev, true);
+    }
+}
+
+/// <summary>
+/// Raised when a map with dynamic weather switches from one weather state to another.
+/// </summary>
+[ByRefEvent]
+public readonly record struct DynamicWeatherUpdateEvent(EntityUid DynamicWeather, EntProtoId<WeatherStatusEffectComponent>? PreviousState, EntProtoId<WeatherStatusEffectComponent>? NextState);
